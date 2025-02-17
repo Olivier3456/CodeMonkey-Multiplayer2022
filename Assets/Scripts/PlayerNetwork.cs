23ce9073f2c9c63d8fa327a8a48050ecb7b666d9
Olivier3456 <olivier.larrey@gmail.com>
2025-02-17 20:55:19 +0100

    => 43 min. NetworkVariables and RPC

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,3 +1,4 @@
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -5,9 +6,59 @@ using UnityEngine;
 // Inherits from NetworkBehaviour instead of MonoBehaviour.
 public class PlayerNetwork : NetworkBehaviour
 {
+    // initialize NetworkVariable at declaration
+    private NetworkVariable<MyCustomData> myCustomData = new NetworkVariable<MyCustomData>(
+                                                                         new MyCustomData { _int = 1, _bool = false },
+                                                                         NetworkVariableReadPermission.Everyone,
+                                                                         NetworkVariableWritePermission.Owner);
+    // by default, it is: read by everyone, write only by server
+
+
+
+    public struct MyCustomData : INetworkSerializable
+    {
+        public int _int;
+        public bool _bool;
+
+        // String is ref type, so we can't use it, but we can use FixedString.
+        // 128Bytes is for the max length of the string: here, 128 signs.
+        // Works from 32 to 4096 bytes.
+        public FixedString128Bytes message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
+    }
+
+
+    public override void OnNetworkSpawn()
+    {
+        myCustomData.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) =>
+        {
+            Debug.Log($"Client with Id {OwnerClientId} just changed MyCustomData. Previous _int value was: {previousValue._int}. New _int value is: {newValue._int}. Message is: {newValue.message}");
+        };
+    }
+
+
     private void Update()
     {
         if (!IsOwner) return;
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            myCustomData.Value = new MyCustomData { _int = Random.Range(0, 100), _bool = false, message = "You have a new message!" };
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            ulong targetClientId = 1;
+            TestOtherRpc(RpcTarget.Single(targetClientId, RpcTargetUse.Temp));
+        }
+
+
 
         Vector3 moveDir = new Vector3(0, 0, 0);
 
@@ -19,5 +70,21 @@ public class PlayerNetwork : NetworkBehaviour
         float moveSpeed = 3f;
 
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+
+
+    // Here, the rpc will execute HOST (= server) side, even if triggered by a CLIENT!
+    // But OwnerClientId is the one of the instance calling the function
+    // (=> client if called by a client)
+    [Rpc(SendTo.Server)]
+    private void TestRpc() // rpc function name must finish by rpc
+    {
+        Debug.Log($"{OwnerClientId} called function TestRpc.");
+    }
+
+    [Rpc(SendTo.SpecifiedInParams)]
+    private void TestOtherRpc(RpcParams rpcParams = default)
+    {
+        Debug.Log($"{OwnerClientId} called function TestOtherRpc.");
     }
 }
