c8d0cb38a7e299f2f5bd42af98bd043de1642c4e
Olivier3456 <olivier@lewebpedagogique.com>
2025-02-17 17:08:59 +0100

    First commit => 21 min

---
+++ Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,23 @@
+using Unity.Netcode;
+using UnityEngine;
+
+
+// Inherits from NetworkBehaviour instead of MonoBehaviour.
+public class PlayerNetwork : NetworkBehaviour
+{
+    private void Update()
+    {
+        if (!IsOwner) return;
+
+        Vector3 moveDir = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
+        if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+
+        float moveSpeed = 3f;
+
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+}
